46e16b02bbd0f31944adbd19ec11f5a5e3c2594f
Pranav833 <salunkhepranav833@gmail.com>
2026-01-02 19:50:37 +0530

    setup

---
+++ solution.cpp
@@ -0,0 +1,13 @@
+// LC: 0000 placeholder-slug
+// Title: Placeholder Title
+// Difficulty:
+// Date:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // write here
+};
