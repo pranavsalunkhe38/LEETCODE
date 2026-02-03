1403fda1cae01b655065a432f5634898bfc16175
Pranav833 <salunkhepranav833@gmail.com>
2026-02-03 15:26:45 +0530

    03 Feb 2026 3637 Trionic Array

---
+++ solution/3637_Trionic_Array.cpp
@@ -0,0 +1,27 @@
+// LC: 3637 Trionic Array I
+// Title: Trionic Array I
+// Difficulty: Easy
+// Date: 03 Feb 2026
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size(), i = 1;
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int p = i - 1;
+        while (i < n && nums[i - 1] > nums[i]) {
+            i++;
+        }
+        int q = i - 1;
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int flag = i - 1;
+        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+    }
+};
