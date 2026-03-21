2133592c9f971452c0dd613a8982b39cf964f0d8
Pranav833 <salunkhepranav833@gmail.com>
2026-03-21 11:49:46 +0530

    21 March 2026 3643 Flip Square Submatrix Vertically

--- solution/3643_Flip_Square_Submatrix_Vertically.cpp
+++ solution/3643_Flip_Square_Submatrix_Vertically.cpp
@@ -1,25 +1,12 @@
-// LC: 3643 Flip Square Submatrix Vertically
-// Title: Flip Square Submatrix Vertically
-// Difficulty: Easy
-// Date: 21 March 2026
+// LC: 0000 placeholder-slug
+// Title: Placeholder Title
+// Difficulty:
+// Date:
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> reverseSubmatrix(vector<vector<int>>& grid, int x, int y, int k) {
-
-        for(int i = 0; i < k/2; i++){
-            for(int j=0; j < k ; j++){
-               
-
-                swap(grid[x+i][y+j], grid[x+k-1-i][y+j]);
-
-            }
-        }
-
-        return grid;
-        
-    }
+    // write here
 };
