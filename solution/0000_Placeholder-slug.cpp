b1ba5f7d00c49ef8e2b1c0cf1e498a5deb41a4fc
Pranav Salunkhe <pranav.salunkhe38@gmail.com>
2026-05-04 10:05:41 +0530

    "04 May 2026 0000 Placeholder-slug"

--- solution/0000_Placeholder-slug.cpp
+++ solution/0000_Placeholder-slug.cpp
@@ -1,102 +1,33 @@
 // LC: 0000 Placeholder-slug
 // Title: Placeholder-slug
 // Difficulty:
-// Date: 16 April 2026
+// Date: 04 May 2026
 
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    int func(vector<int>& v, int& idx, int& n){
-            // cout<<idx<<"*"<<endl;
-
-            // for(auto i: v){
-            //     cout<<i<<" ";
-            // }
-            // cout<<endl;
-    
-        int sz = v.size();
-        if(sz == 1){
-            return -1;
+    void print(vector<vector<int>>& matrix){
+        for(auto& i: matrix){
+            for(auto& j: i){
+                cout<<j<<" ";
+            }
+            cout<<endl;
         }
-
-        int prev = (idx - 1 + sz) % sz;
-        int nxt = (idx + 1) % sz;
-
-        int curr = v[idx];
-        int back = v[prev];
-        int next = v[nxt];
-
-        int ans = INT_MAX;
-
-        // if(curr - back < ans || n - (curr - back) < ans){
-        //     ans = back;
-        // }
-
-        // if(next - curr < ans || n - (next - curr) < ans){
-        //     ans = next;
-        // }
-
-        // cout<<back<<" "<<next<<endl;
-        
-
-
-        ans = min(ans, min(abs(curr - back),(n - abs(curr - back))));
-        ans = min(ans, min(abs(next - curr),(n - abs(next - curr))));
-
-        return ans == INT_MAX ? -1 : ans;
-
     }
-
-    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        map<int, vector<int>> mp;
-        int n = nums.size();
-
-        for(int i=0; i < nums.size(); i++){
-            mp[nums[i]].push_back(i);
-        }
-
-        // for(auto i: mp){
-        //     cout<<i.first<<endl;
-        //     for(auto j: i.second){
-        //         cout<<j<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-
-        vector<int> ans;
-
-        for(auto i: queries){
-            vector<int>& vec = mp[nums[i]];
-            int idx = lower_bound(vec.begin(), vec.end(), i) - vec.begin();
-
-
-        int sz = vec.size();
-        if(sz == 1){
-            ans.push_back(-1);
-            continue;
-        }
-
-        int prev = (idx - 1 + sz) % sz;
-        int nxt = (idx + 1) % sz;
-
-        int curr = vec[idx];
-        int back = vec[prev];
-        int next = vec[nxt];
-
-        int res = INT_MAX;
-
-
-        res = min(res, min(abs(curr - back),(n - abs(curr - back))));
-        res = min(res, min(abs(next - curr),(n - abs(next - curr))));
-
+    void rotate(vector<vector<int>>& matrix) {
         
-            ans.push_back(res == INT_MAX ? -1 : res);
+        int n = matrix.size();
+        for(int i=0; i < n; i++){
+            for(int j= i; j < n; j++){
+                // cout<<i<<" "<<j<<endl;
+                swap(matrix[i][j], matrix[j][i]);
+                // print(matrix);
+            }
 
+            reverse(matrix[i].begin(), matrix[i].end());
         }
-
-        return ans;
         
     }
 };
